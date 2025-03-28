0f835f1c80d45571aac168f710c9b8076f74b8ab
HarryMaguireSlabhead <johnsonqin0@gmail.com>
2025-03-27 20:54:56 -0400

    song_input function
    User enters a song and artist, and a song from the dataset is returned

---
+++ song_input_test.py
@@ -0,0 +1,47 @@
+import csv
+import difflib
+
+
+def get_song(song: str, artist: str, filename: str) -> str:
+    """
+    Return the song in <filename> that is most similar to <song> by name and artist, which are inputted by the user.
+
+    If no similar songs are found, return 'No song found'.
+
+    Preconditions:
+        - filename is in the format of the Spotify song dataset (see Github for reference)
+    """
+    with open(filename, encoding="utf8") as csvfile:
+        data = csv.reader(csvfile)
+        headers = (next(data))  # Headers
+        rows = list(data)  # Converting csv data to a list of lists, we can iterate through data more than once
+        list_of_songs = [row[0] for row in rows]
+        list_of_artists = [row[1] for row in rows]
+
+    close_songs = difflib.get_close_matches(song, list_of_songs)  # Song titles similar to user's input
+    close_artists = difflib.get_close_matches(artist, list_of_artists)  # Song artists similar to user's input
+
+    if not close_songs or not close_artists:  # There are no close songs or artists to the user's input
+        return 'No song found'
+
+    score_to_song = {}
+    score_to_artist = {}
+    for i in range(len(list_of_artists)):
+        if list_of_songs[i] in close_songs and list_of_artists[i] in close_artists:
+            # Both the song and artist are similar to the user's inputs
+
+            # Return how similar a song is to the user's input, textwise
+            score = difflib.SequenceMatcher(None, list_of_songs[i], song).ratio()
+            # Mapping how similar a song is to the user's input to the song itself
+            score_to_song[score] = list_of_songs[i]
+            score_to_artist[score] = list_of_artists[i]
+
+    # None of the songs/artists were similar
+    if not score_to_song or not score_to_artist:
+        return 'No song found'
+
+    # Return the most similar song
+    return score_to_song[max(score_to_song.keys())] + " by " + score_to_artist[max(score_to_artist.keys())]
+
+# I added the 'by artist' part for testing purposes
+# If we only need the song name (as that's the format of the csv), then remove this part
