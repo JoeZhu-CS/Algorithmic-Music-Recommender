c611dfc0a1f81e103bcee5d6ba01dc02a244a87c
HarryMaguireSlabhead <johnsonqin0@gmail.com>
2025-03-30 12:03:29 -0400

    song_input function
    fixed bugs

--- song_input_test.py
+++ song_input_test.py
@@ -12,38 +12,33 @@ def get_song(song: str, artist: str, filename: str) -> str | None:
         - filename is in the format of the Spotify song dataset (see Github for reference)
         - filename is a valid dataset
     """
-    try:
-        with open(filename, encoding="utf8") as csvfile:
-            data = csv.reader(csvfile)
-            headers = (next(data))  # Headers
-            rows = list(data)  # Converting csv data to a list of lists, we can iterate through data more than once
-            list_of_songs = [row[0].lower() for row in rows]
-            list_of_artists = [row[1].lower() for row in rows]
+    with open(filename, encoding="utf8") as csvfile:
+        data = csv.reader(csvfile)
+        headers = (next(data))  # Headers
+        rows = list(data)  # Converting csv data to a list of lists, we can iterate through data more than once
+        list_of_songs = [row[0].lower() for row in rows]
+        list_of_artists = [row[1].lower() for row in rows]
 
-        close_songs = difflib.get_close_matches(song.lower(), list_of_songs)  # Song titles similar to user's input
-        close_artists = difflib.get_close_matches(artist.lower(), list_of_artists)  # Song artists similar to user's input
+    close_songs = difflib.get_close_matches(song.lower(), list_of_songs)  # Song titles similar to user's input
+    close_artists = difflib.get_close_matches(artist.lower(), list_of_artists)  # Song artists similar to user's input
+    if not close_songs or not close_artists:  # There are no close songs or artists to the user's input
+        raise ValueError('No song found')
 
-        if not close_songs or not close_artists:  # There are no close songs or artists to the user's input
-            raise ValueError('No song found')
+    score_to_song = {}
+    score_to_artist = {}
+    for i in range(len(list_of_artists)):
+        if list_of_songs[i] in close_songs and list_of_artists[i] in close_artists:
+            # Both the song and artist are similar to the user's inputs
 
-        score_to_song = {}
-        score_to_artist = {}
-        for i in range(len(list_of_artists)):
-            if list_of_songs[i] in close_songs and list_of_artists[i] in close_artists:
-                # Both the song and artist are similar to the user's inputs
+            # Return how similar a song is to the user's input, textwise
+            score = difflib.SequenceMatcher(None, list_of_songs[i], song.lower()).ratio()
+            # Mapping how similar a song is to the user's input to the song itself
+            score_to_song[score] = list_of_songs[i]
+            score_to_artist[score] = list_of_artists[i]
 
-                # Return how similar a song is to the user's input, textwise
-                score = difflib.SequenceMatcher(None, list_of_songs[i], song.lower()).ratio()
-                # Mapping how similar a song is to the user's input to the song itself
-                score_to_song[score] = list_of_songs[i]
-                score_to_artist[score] = list_of_artists[i]
+    # None of the songs/artists were similar
+    if not score_to_song or not score_to_artist:
+        raise ValueError('No song found')
 
-        # None of the songs/artists were similar
-        if not score_to_song or not score_to_artist:
-            raise ValueError('No song found')
-
-        # Return the most similar song
-        return score_to_song[max(score_to_song.keys())] # + " by " + score_to_artist[max(score_to_artist.keys())]
-
-    except ValueError:
-        print("No song found")
+    # Return the most similar song
+    return score_to_song[max(score_to_song.keys())] # + " by " + score_to_artist[max(score_to_artist.keys())]
